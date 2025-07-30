dec443d67fa6111df1adfa80482fc78eb7ce64e5
John Hu <johnhu1235@gmail.com>
2025-07-29 20:49:02 -0400

    getting 3js to wrk

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.152.2/examples/jsm/loaders/GLTFLoader.js';
 
+//3JS
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
